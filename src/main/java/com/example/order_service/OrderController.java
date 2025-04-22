cf61bfde9749645bfb82578edd67f4b01e188220
immanuel <immanuelsundar07@gmail.com>
2025-04-22 14:40:01 +0530

    Internal Added internal call to book-service changes in order controller file and modified dockerfile

--- src/main/java/com/example/order_service/OrderController.java
+++ src/main/java/com/example/order_service/OrderController.java
@@ -2,13 +2,21 @@ package com.example.order_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class OrderController {
 
-    // This will respond to GET requests at /orders
+    RestTemplate restTemplate = new RestTemplate(); // For internal HTTP call
+
     @GetMapping("/orders")
     public String getOrders() {
-        return "This is a list of orders!";
+        // book-service ku internal ah HTTP GET call panrom
+        //old one --String bookServiceResponse = restTemplate.getForObject("http://book-service", String.class);
+        String bookServiceResponse = restTemplate.getForObject("http://book-service:3000/books", String.class);
+        // Result merge pannitu return panrom
+        return "Order Service: List of orders!\nBook Service says: " + bookServiceResponse;
     }
 }
+
+
